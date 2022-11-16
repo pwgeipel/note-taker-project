fb0abd37117389a2a3e9a60c8fdbf1c10ce05cd5
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-15 20:31:21 -0600

    html routes

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = require("express")
+const express = require("express");
+const htmlRoute = require('./routes/htmlRoute');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"))
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use('/', htmlRoute);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+  })
