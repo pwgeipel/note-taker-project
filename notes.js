b4abb7aaddb8f5768e7977d633f2c6cb1dc75038
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-16 23:16:04 -0600

    notes.js

--- notes.js
+++ notes.js
@@ -5,6 +5,21 @@ function refreshDb(deleteNote, notesArray) {
     const deleteNote = deleteNote;
     for (let i = 0; i , notesArray.length; i++) {
         notesArray.splice(i, 1);
+        fs.writeFileSync(path.join(__dirname, "db/db.json"),
+        JSON.stringify({notes: notesArray}, null, 2), err => {
+            if (err) {
+                throw err;
+            }
+        })
         
     }
 }
+
+function makeNewNote(newNote, notesArray) {
+    const newNote = newNote;
+    fs.writeFileSync(path.join(__dirname, "db/db.json"),
+    JSON.stringify({notes: notesArray}, null, 2));
+    return newNote;
+}
+
+module.exports = {refreshDb, makeNewNote}
