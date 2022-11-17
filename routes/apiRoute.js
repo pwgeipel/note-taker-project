f979a41179be94893445c3fea8e4a4d4b5452e8b
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-17 15:42:32 -0600

    router.delete

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,16 +4,27 @@ const { v4: uuidv4 } = require('uuid');
 const {refreshDb, makeNewNote} = require('../notes')
 
 router.get('/notes', (req, res) => {
-    let results = db;
     res.json(results);
+});
 
-    router.post('/notes', (req, res) => {
-        req.body.id = uuidv4;
-        const newNote = makeNewNote(req.body, notes);
-        res.json(newNote);
-    });
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = makeNewNote(req.body, notes);
+    res.json(newNote);
+});    
 
-    
-})
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let notes
+
+    db.map((element, index) => {
+        if (element.id == id) {
+            note = element;
+            db.splice(index, 1);
+            refreshDb(db);
+            return res.json(note);
+        }
+    })
+});
 
 module.exports = router;
