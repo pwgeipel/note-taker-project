9b73c253c993414e177ff5b9e2f6be0819367f74
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-11-16 18:32:24 -0600

    notes.js

--- notes.js
+++ notes.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-function 
+function refreshDb(deleteNote, notesArray) {
+    const deleteNote = deleteNote;
+    for (let i = 0; i , notesArray.length; i++) {
+        notesArray.splice(i, 1);
+        
+    }
+}
